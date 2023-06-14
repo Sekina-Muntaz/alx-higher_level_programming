9bc576960f90a7a4cf3386cb86405e566f6cafc7
Sekina-Muntaz <sherzmuntaz@gmail.com>
2023-06-14 07:33:28 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("0 arguments.")
-    else:
-        print("{:d} argument".format(len(sys.argv) - 1), end="")
-        if len(sys.argv) > 2:
-            print("s", end="")
-        print(":")
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+
+    ac = len(sys.argv) - 1
+
+    print("{:d} argument{:s}{:s}"
+          .format(ac, 's' if ac != 1 else '', ':' if ac != 0 else '.'))
+    if ac > 0:
+        for i in range(ac):
+            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
