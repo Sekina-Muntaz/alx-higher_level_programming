44c10991abf29b030f3ab476d57e7abac5d20f39
Sekina-Muntaz <sherzmuntaz@gmail.com>
2023-06-26 12:14:32 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    result = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            result += 1
+        except:
+            continue
+    print()
+    return result
